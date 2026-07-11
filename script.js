3ec86915ccf711043bbd984da671a0193ad99f07
Zzzzjutt <zubairali6535668@gmail.com>
2026-07-11 01:05:56 -0700

    Add multiple images and enhance index.html with new sections and animations

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-(10, 165, 105)
+let lastScroll = 0;
+const navbar = document.querySelector(".nav-bar");
+
+window.addEventListener("scroll", () => {
+
+    const currentScroll = window.pageYOffset;
+
+    if(currentScroll > lastScroll && currentScroll > 100){
+        navbar.style.top = "-100px";
+    }else{
+        navbar.style.top = "0";
+    }
+
+    lastScroll = currentScroll;
+
+});
