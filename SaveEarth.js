f303c220be54f5e5727b5cae8999ece1fd04ef67
Zzzzjutt <zubairali6535668@gmail.com>
2026-07-13 19:53:32 -0700

    Home Page Done

---
+++ SaveEarth.js
@@ -0,0 +1,14 @@
+let lastScroll = 0;
+const navbar = document.querySelector(".nav-bar");
+
+window.addEventListener("scroll", () => {
+    const currentScroll = window.pageYOffset;
+
+    if (currentScroll > lastScroll && currentScroll > 100) {
+        navbar.style.top = "-100px";
+    } else {
+        navbar.style.top = "0";
+    }
+
+    lastScroll = currentScroll;
+});
