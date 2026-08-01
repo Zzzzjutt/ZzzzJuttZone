fbb45cd96bd95e058ba581177400ad8e339dfd40
Sajjad Ahmad <zubairali6535668@gmail.com>
2026-08-01 15:51:15 +0300

    Creative For Earth Page Done

--- SaveEarth.js
+++ SaveEarth.js
@@ -12,3 +12,56 @@ window.addEventListener("scroll", () => {
 
     lastScroll = currentScroll;
 });
+
+emailjs.init("soqociZpnSL7jqumd");
+
+const form = document.getElementById("contactForm");
+const btn = document.querySelector(".contact-btn");
+const toast = document.getElementById("toast");
+
+form.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    // Button Disable
+    btn.disabled = true;
+    btn.innerHTML = "Sending...";
+
+    emailjs.sendForm(
+        "service_8gnjyhd",
+        "template_5y3m78p",
+        this
+    )
+
+        .then(() => {
+
+            // Toast Show
+            toast.classList.add("show");
+
+            // Form Reset
+            form.reset();
+
+            // Button Enable
+            btn.disabled = false;
+            btn.innerHTML = "Send Message";
+
+            setTimeout(() => {
+
+                toast.classList.remove("show");
+
+            }, 3000);
+
+        })
+
+        .catch((error) => {
+
+            console.log(error);
+
+            btn.disabled = false;
+            btn.innerHTML = "Send Message";
+
+            alert("Failed to send message.");
+
+        });
+
+});
