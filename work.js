865ec87af618cae0d3577f76ab7155431a7f3c7b
Sajjad Ahmad <zubairali6535668@gmail.com>
2026-08-12 13:41:52 +0500

    Add more pages

---
+++ work.js
@@ -0,0 +1,21 @@
+const textElement = document.getElementById("changingText");
+
+const texts = [
+    "🎨 MY ART",
+    "✏️ MY DESIGN",
+    "💡 MY IDEAS",
+    "🖌️ MY CREATIONS",
+    "🚀 MY WORK"
+];
+
+let currentIndex = 0;
+
+textElement.addEventListener("animationiteration", () => {
+    currentIndex++;
+
+    if (currentIndex >= texts.length) {
+        currentIndex = 0;
+    }
+
+    textElement.textContent = texts[currentIndex];
+});
